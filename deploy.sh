e41e830e860c247a00c4df30fded52183f7fde23
ykk.jin <ykky.jin@gmail.com>
2021-12-01 15:43:12 -0500

    add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,29 @@
-#!/bin/sh
+#!/bin/bash
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+msg="Rebuilding site $(date)"
+if [ -n "$*" ]
+then
+  msg="$*"
+fi
+
+echo ""
+echo "$"
+hugo -D
+
+cd public
+echo ""
+echo ""
+echo "Committing changes to $(pwd)"
+git add .
+git commit -m "${msg}"
+git push
+
+cd ..
+echo ""
+echo ""
+echo "Committing changes to $(pwd)"
+git add public
+git commit -m "${msg}"
+git push
